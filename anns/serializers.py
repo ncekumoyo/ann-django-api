7fbbe400917014330211c8760357778c1e95a302
ncekumoyo <nhmoyo7@gmail.com>
2023-04-13 18:19:22 +0200

    update serializer fields

--- anns/serializers.py
+++ anns/serializers.py
@@ -3,12 +3,16 @@ from .models import Ann
 
 
 class AnnSerializer(serializers.ModelSerializer):
-    entity_name = serializers.StringRelatedField(
-        source='entity')
-    department_name = serializers.StringRelatedField(
-        source='department')
+    entity_name = serializers.CharField(
+        source='entity.name')
+    department_name = serializers.CharField(
+        source='department.name')
+    entity_logo = serializers.CharField(
+        source='entity.logo')
+    department_logo = serializers.CharField(
+        source='department.logo')
 
     class Meta:
         model = Ann
-        fields = ['id', 'date_created', 'entity_name', 'department_name',
+        fields = ['id', 'date_created', 'entity_name', 'entity_logo', 'department_name', 'department_logo',
                   'title', 'venue', 'start', 'end', 'info', 'poster']
